e69dbb784ad62d18622e64e139f4f3466b99e932
chan3785 <dirtyface1234@naver.com>
2025-06-24 01:03:15 +0900

    feature(api,backend): todo API PUT 메서드 구현
    - src/app/api/todo/route.ts: PUT 메서드 추가, ToDo 리스트 전체 덮어쓰기 및 성공/실패 응답 처리
    - src/lib/backend/write-firebase.ts: writeUserTodoList에서 Firestore 저장 후 데이터 검증 및 저장된 tasks 반환 로직 추가

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,14 +1,33 @@
-<<<<<<< HEAD
-import { getUserTodoList } from "@/lib/read-firebase";
-=======
 import { getUserTodoList } from "@/lib/backend/read-firebase";
->>>>>>> b0b912f2b17d67dc057464511025a6cb8c0352cf
-export async function GET(request:Request) {
+import { writeUserTodoList } from "@/lib/backend/write-firebase";
+import { NextRequest, NextResponse } from "next/server";
+export async function GET() {
     //fetch data from db
+    try {
     const todolists = getUserTodoList()
-    
-    return new Response(JSON.stringify(todolists), {
+    return new NextResponse(JSON.stringify(todolists), {
         status:200,
         headers: {'Content-Type': 'application/json' }
     })
+    } catch (error) {
+        return NextResponse.json(
+            { success: false, error: (error as Error).message },
+            { status: 500 }
+          );
+    }
 }
+
+
+export async function PUT(request: NextRequest) {
+  try {
+    const todolists = await request.json();
+    await writeUserTodoList(todolists);
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
+
