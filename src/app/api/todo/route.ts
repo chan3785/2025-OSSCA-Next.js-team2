4a99f8668b41e856087f953cdc5178b059d58723
chan3785 <dirtyface1234@naver.com>
2025-06-24 15:42:40 +0900

    bugfix(api): 인증 기반으로 ToDo 리스트 조회 및 저장 기능 개선
    - src/app/api/todo/route.ts: GET 및 PUT 메서드에서 idToken을 사용하여 인증 처리 추가
    - src/lib/backend/read-firebase.ts: 사용자 인증을 위한 idToken 검증 로직 추가 및 Firestore에서 ToDo 리스트 조회 방식 변경
    - src/lib/backend/write-firebase.ts: 사용자 인증을 위한 idToken 검증 로직 추가 및 ToDo 리스트 저장 방식 변경
    - 서버 실행 admin sdk로 전환

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,27 +1,33 @@
 import { getUserTodoList } from "@/lib/backend/read-firebase";
 import { writeUserTodoList } from "@/lib/backend/write-firebase";
 import { NextRequest, NextResponse } from "next/server";
-export async function GET() {
-    //fetch data from db
-    try {
-    const todolists = getUserTodoList()
-    return new NextResponse(JSON.stringify(todolists), {
-        status:200,
-        headers: {'Content-Type': 'application/json' }
-    })
-    } catch (error) {
-        return NextResponse.json(
-            { success: false, error: (error as Error).message },
-            { status: 500 }
-          );
-    }
+
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get('authorization');
+  const idToken = authHeader?.split(' ')[1];
+  if (!idToken) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+  try {
+    const todolists = await getUserTodoList(idToken);
+    return NextResponse.json(todolists, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: (error as Error).message },
+      { status: 500 }
+    );
+  }
 }
 
-
 export async function PUT(request: NextRequest) {
+  const authHeader = request.headers.get('authorization');
+  const idToken = authHeader?.split(' ')[1];
+  if (!idToken) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
   try {
     const todolists = await request.json();
-    await writeUserTodoList(todolists);
+    await writeUserTodoList(idToken, todolists);
     return NextResponse.json({ success: true });
   } catch (error) {
     return NextResponse.json(
